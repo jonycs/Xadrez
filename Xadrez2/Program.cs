f8aad7df8c7fff33f3d7b5e0dce1f8b8f4711936
Jonatas <jonatascstambouli@gmail.com>
2020-08-06 11:19:02 -0300

    Start

---
+++ Xadrez2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xadrez2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
