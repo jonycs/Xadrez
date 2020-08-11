0f5cd2072c11373debcebb1c54340b95033d0707
Jonatas <jonatascstambouli@gmail.com>
2020-08-11 10:05:13 -0300

    Testes de movimentos das pecas

--- Xadrez2/Program.cs
+++ Xadrez2/Program.cs
@@ -16,6 +16,12 @@ namespace tabuleiro
                     Tela.ImprimirTabuleiro(Partida.tab);
                     Console.Write("Origem: ");
                     Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+
+                    bool[,] PosicoesPossiveis = Partida.tab.Peca(origem).MovimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.ImprimirTabuleiro(Partida.tab, PosicoesPossiveis);
+                    
                     Console.Write("Destino: ");
                     Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
                     Partida.ExecutaMovimento(origem, destino);
--- Xadrez2/Xadrez/Rei.cs
+++ Xadrez2/Xadrez/Rei.cs
@@ -13,5 +13,78 @@ namespace Xadrez
         {
             return "R";
         }
+
+        private bool PodeMover(Posicao pos)
+        {
+            Peca p = Tab.Peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //ACIMA
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna);
+            if(Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            
+
+            //ABAIXO
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //ESQUERDA
+            pos.DefinirValores(Posicao.Linha, Posicao.Coluna -1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //DIREITA
+            pos.DefinirValores(Posicao.Linha, Posicao.Coluna + 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //NE
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //SE
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //SO
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //NO
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            return mat;
+        }
     }
 }
